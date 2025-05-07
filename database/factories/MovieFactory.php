f33eaa0de9dc49e29ef91b0d056a7a3b8a0935d3
suryaandexa <suryalanggaandexa@gmail.com>
2025-05-07 16:01:33 +0700

    seeder and factory

--- database/factories/MovieFactory.php
+++ database/factories/MovieFactory.php
@@ -3,6 +3,9 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Category;
+use Illuminate\Support\Str;
+use App\Models\Movie;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Movie>
@@ -16,8 +19,19 @@ class MovieFactory extends Factory
      */
     public function definition(): array
     {
+        $title = fake()->sentence(rand(3, 6)); // Judul film acak
+        $slug = Str::slug($title); // Membuat slug dari judul
+
         return [
-            //
+            'title' => $title, // Judul film
+            'slug' => $slug, // Slug dari judul
+            'synopsis' => fake()->paragraph(rand(5, 10)), // Deskripsi film
+            'category_id' => Category::inRandomOrder()->first(), // ID kategori acak
+            'year' => fake()->year(), // Tahun rilis film
+            'actors' => fake()->name() . ', ' . fake('id')->name(), // Nama dua aktor
+            'cover_image' => 'https://picsum.photos/seed/' . Str::random(10) . '/480/640', // Gambar sampul film
+            'created_at' => now(), // Waktu pembuatan
+            'updated_at' => now(), // Waktu pembaruan
         ];
     }
 }
