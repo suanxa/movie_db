10d10da4775c5b53065b3d70297059a4a5964c0b
suryaandexa <suryalanggaandexa@gmail.com>
2025-05-14 14:03:48 +0700

    homepage

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,8 +3,72 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class MovieController extends Controller
 {
-    //
+    public function index()
+    {
+        $movies = Movie::select('title', 'synopsis', 'cover_image')->paginate(6);
+        return view('movie.index', compact('movies'));
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id) {
+        $movie = Movie::findOrFail($id);
+        return view('movie.show', compact('movie'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Categories $categories)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Categories $categories)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Categories $categories)
+    {
+        //
+    }
+
+    public function search(Request $request)
+    {
+        $query = $request->input('query');
+
+        // Query pencarian berdasarkan title
+        $movies = Movie::where('title', 'LIKE', "%{$query}%")->paginate(6);
+
+        return view('movie.index', compact('movies'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [MovieController::class, 'index'])->name('movies.index');
+Route::get('/search', [MovieController::class, 'search'])->name('movies.search');
